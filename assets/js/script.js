0b4345bfacca13fa320f427d08d3b40e0daf2f4b
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-30 02:23:19 -0400

    added initial code for script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const challengeBox = document.querySelector(".challengeBox");
+
+const questionBox = document.querySelector(".questionBox");
+
+const responseAns = document.querySelector(".responseAns");
+
+const resultScorePg = document.querySelector(".resultScorePg");
+
+const hrDiv = document.getElementById('div-hr');
+
